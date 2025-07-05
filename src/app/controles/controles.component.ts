1677a60db447d14c8d6a1de8c3af1ce7517bd897
Nicolás Fagetti <nicolas.moro.fagetti@gmail.com>
2025-07-05 03:27:21 -0300

    Erased old commented code at Controles component.

--- src/app/controles/controles.component.ts
+++ src/app/controles/controles.component.ts
@@ -14,31 +14,7 @@ import { MinistryControlComponent } from '../ministry-control/ministry-control.c
   styleUrl: './controles.component.scss'
 })
 export class ControlesComponent {
-  // players : Player[] = [];
-  // ministerios : Ministry[] = [];
-  // politicos : Politico[] = [];
-  // politicosAssigned : Politico[] = [];
 
-  // possibleValues : number[] = [];
-  // playerAssign : Player = new Player;
-  // politicoAssign : Politico = new Politico;
-  // politicoDeclare : Politico = new Politico;
-  // assignValue! : number;
-  // assignRequest : InfluenceRequest = new InfluenceRequest;
-  // assignConfirmation : InfluenceAssigned = new InfluenceAssigned;
-  // declareRequest : InfluenceRequest = new InfluenceRequest;
-  // declareConfirmation : InfluenceDeclared = new InfluenceDeclared;
-  
-  // ministerioObjetivo : Ministry = new Ministry;
-  // ministerioOcupado : Ministry = new Ministry;
-  // ministerioVaciado : Ministry = new Ministry;
-  // ministroAsignado : Politico = new Politico;
-  // ministroEliminado : Politico = new Politico;
-
-  constructor(
-    // private ministryService : MinistryService,
-    // private politicoService : PoliticoService,
-    // private playerService : PlayerService
-  ){}
+  constructor(){}
 
 }
