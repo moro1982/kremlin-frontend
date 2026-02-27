b4234e8751e3e246e816c868eec56ba520a61f5d
Nicolás Fagetti <nicolas.moro.fagetti@gmail.com>
2026-02-27 03:50:49 -0300

    --

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
-import { AssignControlComponent } from './components/assign-control/assign-control.component';
 import { GameComponent } from './components/game/game.component';
 import { BoardComponent } from './components/board/board.component';
 import { GamePageComponent } from './components/game-page/game-page.component';
@@ -15,7 +14,6 @@ export const routes: Routes = [
      { path: '', component: HomeComponent },
      { path: 'login', component: LoginComponent },
      { path: 'register', component: RegisterComponent },
-     { path: 'assign', component: AssignControlComponent },
      { path: 'board', component: BoardComponent },
      { path: 'games', component: GameComponent },
      { path: 'game-list', component: GameListComponent },
